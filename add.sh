4c3e66aa8978434fe1c5de2c92eb9832d196c862
RAM <ramkiransm@gmail.com>
2022-08-15 18:54:18 +0000

    added space to check

--- add.sh
+++ add.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 SUM=`expr $1 + $2`
 echo "sum of $1 and $2 is $SUM"
 
--- initial.sh
+++ initial.sh
@@ -3,7 +3,6 @@ VAR1="BALE"
 VAR2="WALES"
 VAR3="32"
 
-
 echo "$0"
 echo "MY NAME IS $VAR1"
 echo "I AM FROM $VAR2"
